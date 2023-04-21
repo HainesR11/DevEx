fddc4e9b05207e64d09379f86978bbe74839c3e2
HainesR11 <58213900+HainesR11@users.noreply.github.com>
2023-04-21 15:12:49 +0100

    status check update

--- src/StatusCheck.tsx
+++ src/StatusCheck.tsx
@@ -4,7 +4,7 @@ import {SafeAreaView} from 'react-native';
 import useAppStatus from '@DevEx/hooks/useAppStatus';
 
 import LoadingSpinner from './screens/loading';
-import Maintenance from './screens/maintenance';
+import Maintenance from './screens/maintenance/maintenance';
 
 const AppStatusCheck = ({children}: {children: Element}) => {
   const {appStatus} = useAppStatus();
