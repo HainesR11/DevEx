35440ffc39ac43269710ead0e8234a5fc49d6ea9
Rhys Haines <rhys.haines11@gmail.com>
2024-12-11 13:34:15 +0000

    Home Header updated

---
+++ src/components/Headers/CommentsHeader.tsx
@@ -0,0 +1,49 @@
+import React, {Dispatch, SetStateAction} from 'react';
+import {View} from 'react-native-reanimated/lib/typescript/Animated';
+
+import {useThemedStyles} from '@DevEx/hooks/UseThemeStyles';
+
+import {Text} from '../Text/text';
+
+import createStyles from './Header.styles';
+
+type THeaderParams = 'Comments' | 'Likes';
+
+export const CommentViewHeader = (
+  activeHeader: string,
+  onPress: Dispatch<SetStateAction<THeaderParams>>,
+) => {
+  const styles = useThemedStyles(createStyles);
+  return (
+    <View style={styles.commentContainer}>
+      <View
+        style={[
+          styles.textViewContainer,
+          {
+            borderBottomWidth: activeHeader === 'Likes' ? 1 : undefined,
+          },
+        ]}>
+        <Text
+          textStyle={styles.textStyle}
+          bold={activeHeader === 'Likes'}
+          text={'Likes'}
+          onPress={() => onPress('Likes')}
+        />
+      </View>
+      <View
+        style={[
+          styles.textViewContainer,
+          {
+            borderBottomWidth: activeHeader === 'Comments' ? 1 : undefined,
+          },
+        ]}>
+        <Text
+          textStyle={styles.textStyle}
+          bold={activeHeader === 'Comments'}
+          text={'Comments'}
+          onPress={() => onPress('Comments')}
+        />
+      </View>
+    </View>
+  );
+};
--- src/components/Headers/HomeHeader.tsx
+++ src/components/Headers/HomeHeader.tsx
@@ -1,4 +1,4 @@
-import React, {Dispatch, SetStateAction} from 'react';
+import React from 'react';
 import {Image, TouchableOpacity, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {faBell, faChevronLeft} from '@fortawesome/free-solid-svg-icons';
@@ -8,7 +8,7 @@ import {useNavigation} from '@react-navigation/native';
 import {MessagesPlane} from '@DevEx/assets/Icons/Linear/MessagesPlane';
 import {
   MESSAGES_NAVIGATOR,
-  SEARCH_NAVIGATOR,
+  NOTIFICATION_SCREEN,
 } from '@DevEx/constants/screenNames';
 import {useThemedStyles} from '@DevEx/hooks/UseThemeStyles';
 import {THeaderProps, TNavigationProps} from '@DevEx/utils/types/types';
@@ -28,13 +28,14 @@ export const HomeHeader = ({isHomeScreen = true, title}: THeaderProps) => {
       <SafeAreaView edges={['top', 'left', 'right']} style={styles.container}>
         <TouchableOpacity
           style={styles.chevron}
+          testID="go-back-chevron"
           onPress={() => navigation.goBack()}>
           <FontAwesomeIcon icon={faChevronLeft} />
         </TouchableOpacity>
         <Text
           numberOfLines={1}
           textStyle={styles.title}
-          text={title?.title as string}
+          text={title as string}
         />
       </SafeAreaView>
     );
@@ -49,13 +50,15 @@ export const HomeHeader = ({isHomeScreen = true, title}: THeaderProps) => {
       <View style={styles.iconContainer}>
         <NotificationIcon
           count={1}
+          testId="notification-bell-icon"
           icon={faBell}
           size={20}
-          onPress={() => navigation.navigate(SEARCH_NAVIGATOR)}
+          onPress={() => navigation.navigate(NOTIFICATION_SCREEN)}
         />
         <NotificationIcon
           type="Numbered"
           count={1}
+          testId="messages-plane-icon"
           icon={MessagesPlane}
           size={25}
           dotStyle={styles.messagesDot}
@@ -65,44 +68,4 @@ export const HomeHeader = ({isHomeScreen = true, title}: THeaderProps) => {
     </SafeAreaView>
   );
 };
-
-type THeaderParams = 'Comments' | 'Likes';
-
-export const CommentViewHeader = (
-  activeHeader: string,
-  onPress: Dispatch<SetStateAction<THeaderParams>>,
-) => {
-  const styles = useThemedStyles(createStyles);
-  return (
-    <View style={styles.commentContainer}>
-      <View
-        style={[
-          styles.textViewContainer,
-          {
-            borderBottomWidth: activeHeader === 'Likes' ? 1 : undefined,
-          },
-        ]}>
-        <Text
-          textStyle={styles.textStyle}
-          bold={activeHeader === 'Likes'}
-          text={'Likes'}
-          onPress={() => onPress('Likes')}
-        />
-      </View>
-      <View
-        style={[
-          styles.textViewContainer,
-          {
-            borderBottomWidth: activeHeader === 'Comments' ? 1 : undefined,
-          },
-        ]}>
-        <Text
-          textStyle={styles.textStyle}
-          bold={activeHeader === 'Comments'}
-          text={'Comments'}
-          onPress={() => onPress('Comments')}
-        />
-      </View>
-    </View>
-  );
-};
+export default HomeHeader;
